90047771b3d49c26fe3c8de61fad048da4dceeee
Anawrulkabir <mdanawrulkabirfahad123@gmail.com>
2024-06-30 14:11:25 +0600

    Added minor changes

--- src/app/animate/page.jsx
+++ src/app/animate/page.jsx
@@ -28,14 +28,14 @@ export default function Home() {
             setIsHovered(false)
           }}
         >
-          A visual designer - with skills that haven't been replaced by A.I
+          A visual designer - with skills that haven&apos;t been replaced by A.I
           (yet) - making good shit only if the paycheck is equally good.
         </p>
       </motion.div>
 
       <div className={styles.body}>
         <p className="p">
-          I'm a <span className="span">selectively skilled</span> product
+          I&apos;m a <span className="span">selectively skilled</span> product
           designer with strong focus on producing high quality & impactful
           digital experience.
         </p>
